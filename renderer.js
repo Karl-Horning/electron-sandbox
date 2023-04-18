19807bac8b5f9199aa3537fc9b53e60efd2a156d
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2023-04-18 23:12:39 +0100

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -6,7 +6,6 @@ const titleInput = document.getElementById("title");
 
 const func = async () => {
     const response = await window.versions.ping();
-    console.log(response); // prints out 'pong'
 };
 
 func();
